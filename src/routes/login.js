db5181b4823b7f25774763d8972314a239353595
Tyler Haun <me@tylerhaun.com>
2021-07-19 01:07:07 +0000

    added routes for login with password / access token

--- src/routes/login.js
+++ src/routes/login.js
@@ -8,6 +8,11 @@ const routeName = "login"
 module.exports = function(app) {
 
   const controller = new LoginController();
+
+  app.post(`/loginWithPassword`, utils.middlewareMethodWrapper(controller.loginWithPassword.bind(controller), ["body"]))
+  app.post(`/loginWithAccessToken`, utils.middlewareMethodWrapper(controller.loginWithAccessToken.bind(controller), ["body"]))
+
   utils.restRoutes(routeName, controller, app);
+
 
 }
